d5d5a4897156e5772012c21bdfd24abc17a37274
CroiXan <xan.mark.vi@gmail.com>
2025-03-31 17:00:40 -0300

    user api client

---
+++ src/main/java/com/grupo8/jwt/client/UserAPIClient.java
@@ -0,0 +1,31 @@
+package com.grupo8.jwt.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.grupo8.jwt.model.Usuario;
+
+@FeignClient(name = "user-service", url = "https://azuredsy2207g8userfunction.azurewebsites.net/api")
+public interface UserAPIClient {
+
+    @PostMapping(value = "/insertUsuario", consumes = "application/json")
+    String insertUsuario(@RequestBody Usuario usuario);
+
+    @PutMapping(value = "/actualizarUsuario", consumes = "application/json")
+    String actualizarUsuario(@RequestBody Usuario usuario);
+
+    @DeleteMapping("/eliminarUsuario")
+    String eliminarUsuario(@RequestBody String id);
+
+    @GetMapping("/obtenerUsuarios")
+    String obtenerUsuarios();
+
+    @GetMapping("/buscarUsuarioPorRut")
+    String buscarUsuarioPorRut();
+
+
+}
